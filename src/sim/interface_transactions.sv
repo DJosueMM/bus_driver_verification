2412bdae44369de5be5f4a925def3e0b88abe536
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-09-13 19:27:21 -0600

    clases instrucciones_monitor

--- src/sim/interface_transactions.sv
+++ src/sim/interface_transactions.sv
@@ -47,6 +47,58 @@ typedef enum {
     
 } consulta_sb;
 
+class intrucciones_monitor;
+
+    // Definición de los miembros de la clase
+    rand int         max_delay;         // Tiempo máximo de retraso en ciclos de reloj
+    rand int         delay;             // Retraso en ciclos de reloj
+    rand bit         pkg;               // Paquete de datos (ajustar el tamaño según sea necesario)
+    int              send_time;         // Tiempo en el que se envió el paquete
+    int              receive_time;      // Tiempo en el que se recibió el paquete
+    int              dest;              // Destino del paquete
+    int              receiver_monitor;  // Monitor receptor del paquete
+    rand int         id;                // Identificador de la transacción
+    rand bit         payload;           // Payload de la transacción (ajustar el tamaño según sea necesario)
+
+    // Restricciones (si fueran necesarias)
+    // constraint const_delay {
+    //     delay < max_delay;
+    //     delay >= 0;
+    // }
+
+    // Constructor por defecto
+    function new(int max_d = 0, int d = 0, bit[7:0] pkg = 0, int st = 0, int rt = 0, string dst = "", string rcv_mtr = "", int i = 0, bit[15:0] pl = 0);
+        this.max_delay          = max_d;
+        this.delay              = d;
+        this.pkg                = pkg;
+        this.send_time          = st;
+        this.receive_time       = rt;
+        this.dest               = dst;
+        this.receiver_monitor   = rcv_mtr;
+        this.id                 = i;
+        this.payload            = pl;
+    endfunction
+
+    // Método para limpiar los valores
+    function void clean;
+        this.max_delay          = 0;
+        this.delay              = 0;
+        this.pkg                = 0;
+        this.send_time          = 0;
+        this.receive_time       = 0;
+        this.dest               = 0;
+        this.receiver_monitor   = 0;
+        this.id                 = 0;
+        this.payload            = 0;
+    endfunction
+
+    // Método para imprimir los valores
+    function void print(string tag = "");
+        $display("[%g] %s Max Delay=%d Delay=%d pkg=0x%h Send Time=%d Receive Time=%d Dest=%d Receiver Monitor=%d ID=%d Payload=0x%h", 
+                 $time, tag, max_delay, delay, pkg, send_time, receive_time, dest, receiver_monitor, id, payload);
+    endfunction
+
+endclass
 
 
 //Definicion de mailboxes
