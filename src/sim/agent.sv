d3f887bf9bfc9426eda750189952ee3a88c3fe1a
DJosueMM <djosuemm@gmail.com>
2024-09-26 21:43:32 -0600

    packed to unpack

--- src/sim/agent.sv
+++ src/sim/agent.sv
@@ -1,7 +1,7 @@
 class agent # (parameter WIDTH = 16, DRVS = 4);
 
-    mbx_test_agent                                       test_agent_mbx;   // Mailbox del test al agente
-    mbx_agent_driver [DRVS - 1 : 0]                      agnt_drv_mbx;     // Arreglo de mailboxes del agente a cada driver
+    mbx_test_agent   test_agent_mbx;   // Mailbox del test al agente
+    mbx_agent_driver agnt_drv_mbx [DRVS];     // Arreglo de mailboxes del agente a cada driver
     
     int                        num_transacciones;   // Número de transacciones para las funciones del agente
     int                        max_retardo;         // Retardo máximo para las funciones del agente
