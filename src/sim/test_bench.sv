8d379e2d6f34f7433479e1c063ab137871dd687d
DJosueMM <djosuemm@gmail.com>
2024-09-27 21:36:48 -0600

    errores

--- src/sim/test_bench.sv
+++ src/sim/test_bench.sv
@@ -47,11 +47,11 @@ module test_bench;
     
     for (int d = 0; d < DRVS; d++) begin
       //interfases individuales al la interfaz completa
-      test_0.ambiente_inst._compl_dut_if_.pndng[0][d] = test_0.ambiente_inst._driver_dut_if [d].pndng;
+      test_0.ambiente_inst._compl_dut_if_.pndng[0][d] = test_0.ambiente_inst._driver_dut_if [d].pndg;
       //test_0.ambiente_inst._compl_dut_if_.push [0][d] = test_0.ambiente_inst._dut_monitor_if[d].push;
       test_0.ambiente_inst._compl_dut_if_.pop  [0][d] = test_0.ambiente_inst._driver_dut_if [d].pop;
       //test_0.ambiente_inst._compl_dut_if_.dpush[0][d] = test_0.ambiente_inst._dut_monitor_if[d].dpush;
-      test_0.ambiente_inst._compl_dut_if_.Dpop [0][d] = test_0.ambiente_inst._driver_dut_if [d].dpop;
+      test_0.ambiente_inst._compl_dut_if_.D_pop [0][d] = test_0.ambiente_inst._driver_dut_if [d].dpop;
       test_0.ambiente_inst._compl_dut_if_ = final_if;
     end
     fork
