aa3fe59dcedd12f587c0e4360467c8764d23ebf4
DJosueMM <djosuemm@gmail.com>
2024-09-30 18:54:49 -0600

    automatica no en el header

--- src/sim/test_bench.sv
+++ src/sim/test_bench.sv
@@ -45,15 +45,16 @@ module test_bench;
 
     test_0._if = final_if;
         
-    for (automatic int d = 0; d < DRVS; d++) begin
-
-      test_0._driver_dut_if[d] = _driver_dut_if[d];
+    for (int d = 0; d < DRVS; d++) begin
+      
+      automatic int a = d;
+      test_0._driver_dut_if[a] = _driver_dut_if[a];
       //interfases individuales al la interfaz completa
-      test_0.ambiente_inst._compl_dut_if_.pndng[0][d] = test_0.ambiente_inst._driver_dut_if [d].pndg;
-      //test_0.ambiente_inst._compl_dut_if_.push [0][d] = test_0.ambiente_inst._dut_monitor_if[d].push;
-      test_0.ambiente_inst._compl_dut_if_.pop  [0][d] = test_0.ambiente_inst._driver_dut_if [d].pop;
-      //test_0.ambiente_inst._compl_dut_if_.dpush[0][d] = test_0.ambiente_inst._dut_monitor_if[d].dpush;
-      test_0.ambiente_inst._compl_dut_if_.D_pop [0][d] = test_0.ambiente_inst._driver_dut_if [d].dpop;
+      test_0.ambiente_inst._compl_dut_if_.pndng[0][a] = test_0.ambiente_inst._driver_dut_if [a].pndg;
+      //test_0.ambiente_inst._compl_dut_if_.push [0][a] = test_0.ambiente_inst._dut_monitor_if[a].push;
+      test_0.ambiente_inst._compl_dut_if_.pop  [0][a] = test_0.ambiente_inst._driver_dut_if [a].pop;
+      //test_0.ambiente_inst._compl_dut_if_.dpush[0][a] = test_0.ambiente_inst._dut_monitor_if[a].dpush;
+      test_0.ambiente_inst._compl_dut_if_.D_pop [0][a] = test_0.ambiente_inst._driver_dut_if [a].dpop;
     end
     fork
       test_0.run();
