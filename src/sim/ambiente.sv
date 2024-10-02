0cb791af3ebe9e8af8d52416537d73620f114f29
DJosueMM <djosuemm@gmail.com>
2024-10-02 16:28:10 -0600

    monitor

--- src/sim/ambiente.sv
+++ src/sim/ambiente.sv
@@ -5,8 +5,8 @@ class ambiente #(parameter width = 16, parameter DRVS = 8);
     // Declaración de los componentes del ambiente
     agent   #(.WIDTH(width), .DRVS(DRVS)) agent_inst;
 
-    driver  #(.WIDTH(width),.DRVS(DRVS)) driver_inst [DRVS - 1 : 0];
-    //monitor #(.WIDTH(width), .MNT_ID(DRVS)) monitor_inst [DRVS];
+    driver  #(.WIDTH(width),.DRVS(DRVS)) driver_inst  [DRVS - 1 : 0];
+    monitor #(.WIDTH(width),.DRVS(DRVS)) monitor_inst [DRVS - 1 : 0];
 
 
     // Definición de la interface que conecta el DUT
@@ -16,8 +16,8 @@ class ambiente #(parameter width = 16, parameter DRVS = 8);
     // declaración de los mailboxes
     mbx_test_agent       test_agent_mbx;                      // mailbox del test al agente         
     mbx_agent_driver     agent_driver_mbx    [DRVS - 1 : 0];  // mailbox del agente al driver
-    //mbx_driver_checker   driver_checker_mbx  [DRVS - 1 : 0];  // mailbox del driver al checker
-    //mbx_monitor_checker  monitor_checker_mbx [DRVS - 1 : 0];  // mailbox del monitor al checker
+    mbx_driver_checker   driver_checker_mbx  [DRVS - 1 : 0];  // mailbox del driver al checker
+    mbx_monitor_checker  monitor_checker_mbx [DRVS - 1 : 0];  // mailbox del monitor al checker
     //mbx_checker_sb       checker_sb_mbx;                       // mailbox del checker al scoreboard
     //mbx_test_sb          test_sb_mbx;                          // mailbox del test al scoreboard
 
@@ -25,16 +25,18 @@ class ambiente #(parameter width = 16, parameter DRVS = 8);
     function new();
         // Instanciación de los mailboxes
         test_agent_mbx      = new();
-        //driver_checker_mbx  = new();
-        //monitor_checker_mbx = new();
         //checker_sb_mbx      = new();
         //test_sb_mbx         = new();
 
         // Instanciación de los componentes del ambiente
         for (int i = 0; i < DRVS; i++) begin
-            driver_inst[i] = new(i);
-            agent_driver_mbx[i] = new();
-            //monitor_inst[i] = new();
+
+            driver_inst         [i] = new(i);
+            monitor_inst        [i] = new(i);
+
+            agent_driver_mbx    [i] = new();
+            driver_checker_mbx  [i] = new();
+            monitor_checker_mbx [i] = new();
         end
 
         agent_inst = new();
@@ -48,16 +50,15 @@ class ambiente #(parameter width = 16, parameter DRVS = 8);
         agent_inst.agnt_drv_mbx   = agent_driver_mbx;
 
         for (int c = 0; c < DRVS; c++) begin
-            //automatic int c = d;
-            //driver mbx
-            driver_inst[c].agnt_drv_mbx = agent_driver_mbx[c];
-            //driver_inst[c].drv_chkr_mbx = driver_checker_mbx[c];
-            //interface con driver
+          
+            //driver
+            driver_inst[c].agnt_drv_mbx        = agent_driver_mbx  [c];
+            driver_inst[c].drv_chkr_mbx        = driver_checker_mbx[c];
             driver_inst[c].vif_driver_fifo_dut = vif_ambiente_fifo_dut;
-            //monitor mbx
-            //monitor_inst[c].mnt_ckecker_mbx = monitor_checker_mbx[c];
-            //interface con monitor
-            //monitor_inst[c].vif_fifo_dut = _dut_monitor_if [c];
+            
+            //monitor
+            monitor_inst[c].mnt_ckecker_mbx      = monitor_checker_mbx [c];
+            monitor_inst[c].vif_monitor_fifo_dut = vif_ambiente_fifo_dut;
         end
     endfunction
 
@@ -71,11 +72,10 @@ class ambiente #(parameter width = 16, parameter DRVS = 8);
                     automatic int a = j;
                     driver_inst[a].run();
                     $display("[%g] El driver [%g] fue inicializado", $time, a);
+                    monitor_inst[a].run();
+                    $display("[%g] El monitor [%g] fue inicializado", $time, a);
                 join_none
             end
-            
-            //checker_inst.run();
-            //scoreboard_inst.run();
         join_none
     endtask
 endclass
--- src/sim/test_bench.sv
+++ src/sim/test_bench.sv
@@ -2,7 +2,7 @@
 `include "Library.sv"
 `include "interface_transactions.sv"
 `include "driver.sv"
-//`include "monitor.sv"
+`include "monitor.sv"
 `include "agent.sv"
 `include "ambiente.sv"
 `include "secuencer.sv"
@@ -23,8 +23,6 @@ module test_bench;
   secuencer    # (.width(width), .DRVS(DRVS)) test_0;
   dut_compl_if # (.width(width), .drvs(DRVS), .bits(1)) final_if (.clk(clk));
 
-  //fifo_if_in   #(.width(width)) _dut_monitor_if     [DRVS - 1 : 0] (.clk(clk));
-
   //BUS DRIVER    
   bs_gnrtr_n_rbtr # (.pckg_sz(width), .drvrs(DRVS)) DUT (
       .clk      (clk),
@@ -36,7 +34,6 @@ module test_bench;
       .D_push   (final_if.D_push)
   );
   
-  
   always #10 clk = ~clk;
 
   initial begin
@@ -44,9 +41,10 @@ module test_bench;
     clk    = 0;
     test_0 = new();
     for (int w = 0; w < DRVS; w++) begin
-      test_0.ambiente_inst.driver_inst[w].vif_driver_fifo_dut = final_if;
-      test_0.ambiente_inst.agent_inst.vif_agnt_dut = final_if;
-      test_0.vif_test_fifo_dut = final_if;
+      test_0.ambiente_inst.driver_inst[w].vif_driver_fifo_dut   = final_if;
+      test_0.ambiente_inst.agent_inst.vif_agnt_dut              = final_if;
+      test_0.ambiente_inst.monitor_inst[w].vif_monitor_fifo_dut = final_if
+      test_0.vif_test_fifo_dut                                  = final_if;
     end
     
     fork
