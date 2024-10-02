3801c0d3d9fe5a208e87a081973558ec4b32f401
DJosueMM <djosuemm@gmail.com>
2024-10-02 11:55:11 -0600

    test

---
+++ src/sim/test.sh
@@ -0,0 +1,5 @@
+git pull origin develop;
+clear;
+rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
+vcs -Mupdate test_bench.sv  -o salida -full64 -debug_all -kdb -sverilog -l log_test +lint=TFIPC-L;
+./salida;
