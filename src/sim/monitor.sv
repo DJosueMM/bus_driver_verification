b14f08c397415d3154727ef8df89db7c6be63085
DJosueMM <djosuemm@gmail.com>
2024-10-02 21:01:18 -0600

    prints

--- src/sim/monitor.sv
+++ src/sim/monitor.sv
@@ -4,7 +4,7 @@ class monitor # (parameter WIDTH = 16, parameter DRVS = 8);
     virtual dut_compl_if # (.width(WIDTH), .drvs(DRVS), .bits(1)) vif_monitor_fifo_dut;
 
     int mnt_id;
-    //logic [WIDTH - 1 : 0] fifo_out [$];
+
     logic [WIDTH - 1 : 0] aux_reconstr;
 
     function new(int monitor_id = 0);
@@ -20,13 +20,13 @@ class monitor # (parameter WIDTH = 16, parameter DRVS = 8);
         
         @(posedge vif_monitor_fifo_dut.clk);
 
+        $display("[%g] El Monitor [%g] espera por una transacción", $time, mnt_id);
+
         forever begin
 
             instrucciones_driver_monitor #(.WIDTH(WIDTH)) transaction_receive;
             
-            $display("[%g] El Monitor [%g] espera por una transacción", $time, mnt_id);
-
-            @(posedge vif_monitor_fifo_dut.clk);
+            @ (posedge vif_monitor_fifo_dut.clk);
 
                 if (vif_monitor_fifo_dut.push[0][mnt_id] == 1) begin
                                       
@@ -34,9 +34,9 @@ class monitor # (parameter WIDTH = 16, parameter DRVS = 8);
                     
                     transaction_receive= new();
                     
-                    transaction_receive.pkg_id = vif_monitor_fifo_dut.D_push[WIDTH - 1 : WIDTH - 8];
-                    transaction_receive.pkg_payload = vif_monitor_fifo_dut.D_push[WIDTH - 9 : WIDTH - 0];
-                    transaction_receive.receive_time = $time;
+                    transaction_receive.pkg_id           = vif_monitor_fifo_dut.D_push[WIDTH - 1 : WIDTH - 8];
+                    transaction_receive.pkg_payload      = vif_monitor_fifo_dut.D_push[WIDTH - 9 : 0];
+                    transaction_receive.receive_time     = $time;
                     transaction_receive.receiver_monitor = mnt_id;
 
                     if (transaction_receive.pkg_id == 8'b11111111) begin
@@ -48,9 +48,9 @@ class monitor # (parameter WIDTH = 16, parameter DRVS = 8);
                     
                     transaction_receive.delay = 0;
                     transaction_receive.max_delay = 0;
-                    transaction_receive.print("Driver: Transacción reconstruida en el monitor");
+                    transaction_receive.print("Monitor: Transacción reconstruida en el monitor");
                     mnt_ckecker_mbx.put(transaction_receive);
-                    transaction_receive.print("Driver: Transacción enviada al Checker");
+                    transaction_receive.print("Monitor: Transacción enviada al Checker");
                     $display("[%g] El Monitor [%g] envio la transaccion al Checker", $time, mnt_id);
                 end
             end
