26bcb22e93d15ad3c0e3d9691289f9369dfc7234
DJosueMM <djosuemm@gmail.com>
2024-10-05 21:24:20 -0600

    alkdcnaonc

--- src/sim/checker.sv
+++ src/sim/checker.sv
@@ -176,7 +176,7 @@ class checker #(parameter WIDTH = 16, parameter DRVS = 8);
             else begin
 
                 if ((driver_fifo[i].tipo_transaccion == send && driver_fifo[i].pkg_id != driver_fifo[i].receiver_monitor) ||
-                    (driver_fifo[i].tipo_transaccion == send && driver_fifo[i].pkg_id > DRVS) || 
+                    (driver_fifo[i].tipo_transaccion == send && driver_fifo[i].pkg_id >= DRVS - 1) || 
                     (driver_fifo[i].sender_monitor == driver_fifo[i].pkg_id)) begin
                     $display("[%g] Transaccion ilegal o sin proposito correctamente descartada:\n%p", $time, driver_fifo[i]);
                     //driver_fifo.delete(i);
