7b787fa4f1ca6739d2362212c598de671c0ca0e1
DJosueMM <djosuemm@gmail.com>
2024-10-05 20:17:49 -0600

    corrigiendo error de autosend

--- src/sim/agent.sv
+++ src/sim/agent.sv
@@ -288,11 +288,11 @@ class agent # (parameter WIDTH = 16, parameter DRVS = 4);
                          
                         for (int i = 0; i < num_transacciones; i++) begin
 
-                            driver_spec = $urandom_range(0, DRVS - 1);     
+                               
                             transaccion = new();
                             transaccion.randomize();
                             transaccion.max_delay = max_retardo;
-                            id_spec = driver_spec; //revisar asignacion de int a bit
+                            id_spec = $urandom_range(0, DRVS - 1);  
                             transaccion.pkg_id = id_spec;
                             
                             transaccion.print("Agente: transacción auto_send_random creada");
