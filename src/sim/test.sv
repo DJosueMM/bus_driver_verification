8e074c07fc353b7a61f10f33c4bcff7427cdb496
DJosueMM <djosuemm@gmail.com>
2024-10-05 20:53:21 -0600

    tampoco tanto tiempo

--- src/sim/test.sv
+++ src/sim/test.sv
@@ -103,7 +103,7 @@ class test #(parameter width = 16, parameter DRVS = 8);
         
     
         
-        #800000
+        #300000;
         $display("\n////////////////////////////////////////////////////////////////////////////////////////");
         $display("[%g] Test: para finalizar la prueba, se envia tarea de comprobacion final al checker", $time);
         $display("////////////////////////////////////////////////////////////////////////////////////////\n");
